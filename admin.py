67ff95a10c60328ce15b18e2fab8ec2c11da5a39
isaka0104pyweb <162068734+isaka0104pyweb@users.noreply.github.com>
2024-03-11 09:19:56 +0900

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Todo
+
+
+admin.site.register(Todo)
+
+
+
+
+# from .models import Message,Good
+
+# admin.site.register(Message)
+# admin.site.register(Good)
