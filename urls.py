708f705dae41e75507426778549c7448571aadc6
isaka0104pyweb <162068734+isaka0104pyweb@users.noreply.github.com>
2024-03-13 13:33:20 +0900

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
 from .views import TodoDetail, TodoList, TodoCreate, TodoUpdate, TodoDelete
 
+from . import views
+
 urlpatterns = [
     path("", TodoList.as_view(), name="list"),
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),
     path("create/", TodoCreate.as_view(), name="create"),
     path("update/<int:pk>", TodoUpdate.as_view(), name="update"),
     path("delete/<int:pk>", TodoDelete.as_view(), name="delete"),
+
+    # path('', views.index, name='index'),
+    path('sns', views.index, name='index'),
+    path('post', views.post, name='post'),
+    path('goods', views.goods, name='goods'),
+    path('good/<int:good_id>', views.good, name='good'),
+    path('edit/<int:message_id>', views.edit, name='edit'),
+    path('delete/<int:message_id>', views.delete, name='delete'),
+    path('find/<int:num>', views.find, name='find'),
+
+
 ]
-
-
 
 # from django.urls import path
 # from . import views
